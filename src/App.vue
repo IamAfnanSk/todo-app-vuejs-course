6a1ecabf4b2f6aef2b37a2ad411849c249b9d559
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2023-03-27 22:02:24 +0530

    added app.vue code

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
-<script setup>
-import TodoList from "./components/TodoList.vue";
-</script>
-
 <template>
   <TodoList />
 </template>
+
+<script setup>
+import TodoList from "./components/TodoList.vue";
+const todoList = ref(["Learn vue"]);
+
+function updateTodoList(newList) {
+  todoList.value = newList;
+}
+
+provide("todoList", {
+  todoList: readonly(todoList),
+  updateTodoList,
+});
+</script>
 
 <style scoped></style>
