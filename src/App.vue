34b0af99c73ee5f6acee3c178f7dce19a6dbbc55
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2023-03-27 22:10:49 +0530

    added styles and ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,46 @@
-<script setup></script>
+<template>
+  <main class="main">
+    <div class="wrapper-div">
+      <div class="title-div" id="title-div">
+        <h1>Todo List App</h1>
 
-<template></template>
+        <p>{{ 5 }} TODO</p>
+      </div>
+      <div class="todolist-div" id="todo-div">
+        <TodoList />
+      </div>
+    </div>
+  </main>
+</template>
 
-<style scoped></style>
+<script setup>
+import TodoList from "./components/TodoList.vue";
+</script>
+
+<style scoped>
+main {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  padding: 50px;
+  flex-direction: column;
+}
+
+.wrapper-div {
+  width: 500px;
+  padding: 20px;
+  margin: 0 auto;
+  border-radius: 10px;
+  box-shadow: 2px 2px 10px 0 #ccc;
+}
+
+.todolist-div {
+  margin-top: 20px;
+}
+
+.title-div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
